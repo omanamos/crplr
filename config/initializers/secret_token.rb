3f080c7109792e261e492f0827db955cdf91751d
Ryan Oman <omanamos@gmail.com>
2011-10-14 20:54:31 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crplr::Application.config.secret_token = 'e6c7e6cfa368a06e6739f9d3536ec29fd8153440535bbd39317ba90a38dfc203e11fccb8bc36fe72d166231fbf2bdc558284b23f3dcef719617b09a564583071'
